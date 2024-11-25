1f02cb15851fae7651a687045b2d58c90d8e8560
Hosea <karanja.mungai23@gmail.com>
2024-11-25 11:10:09 +0300

    minor fix

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -3,7 +3,7 @@
 def admin_login(username, password):
     # your code here
     if (username == "admin"or username == "ADMIN") and password == "12345":
-        return "Acesss granted"
+        return "Accesss granted"
 
     pass
 print(admin_login("admin", "12345"))
